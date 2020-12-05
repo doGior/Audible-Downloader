6bc00d1fd89fdc368564586802f5356842736d37
Riccardo <rickygiordano03@gmail.com>
2020-12-05 20:29:34 +0100

    Fixed download and added progress bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pathlib, audible, httpx, os
+from tqdm import tqdm
 
 # get download link(s) for book
 def _get_download_link(auth, asin, codec="LC_128_44100_stereo"):
@@ -34,19 +35,23 @@ def _get_download_link(auth, asin, codec="LC_128_44100_stereo"):
 
 
 def download_file(url):
-    r = httpx.get(url)
 
-    try:
-        title = r.headers["Content-Disposition"].split("filename=")[1]
-        filename = pathlib.Path.cwd() / "audiobooks" / title
-    
-        with open(filename, 'wb') as f:
-            for chunk in r.iter_bytes():
-                f.write(chunk)
-        print(f"File downloaded in {r.elapsed}")
-        return filename
-    except KeyError:
-        return "Nothing downloaded"
+    with httpx.stream("GET", url) as r:
+        try:
+            title = r.headers["Content-Disposition"].split("filename=")[1]
+            length = int(r.headers["Content-Length"])
+            filename = pathlib.Path.cwd() / "audiobooks" / title
+
+            with open(filename, 'wb') as f:
+                with tqdm(total=length, unit_scale=True, unit_divisor=1024, unit="B") as progress:
+                    for chunk in r.iter_bytes():
+                        f.write(chunk)
+                        progress.update(len(chunk))
+
+            print(f"File downloaded in {r.elapsed}")
+            return filename
+        except KeyError:
+            return "Nothing downloaded"
 
 
 if __name__ == "__main__":
@@ -56,7 +61,8 @@ if __name__ == "__main__":
         auth = audible.LoginAuthenticator(
             input("Username: "),
             input("Password: "),
-            locale="it")
+            locale="it",
+            register=True)
 
         auth.to_file(
             filename=Auth_file,
@@ -73,12 +79,22 @@ if __name__ == "__main__":
     client = audible.Client(auth)
 
     library = client.get("library", num_results=1000)
+    # asins = [book["asin"] for book in library["items"]]
+
+    # for asin in asins:
+    #     dl_link = _get_download_link(auth, asin)
+
+    #     if dl_link:
+    #         print(f"download link now: {dl_link}")
+    #         status = download_file(dl_link)
+    #         print(f"downloaded file: {status}")
+
     asins = [book["asin"] for book in library["items"]]
+    
 
-    for asin in asins:
-        dl_link = _get_download_link(auth, asin)
+    dl_link = _get_download_link(auth, asins[0])
 
-        if dl_link:
-            print(f"download link now: {dl_link}")
-            status = download_file(dl_link)
-            print(f"downloaded file: {status}")
+    if dl_link:
+        print(f"download link now: {dl_link}")
+        status = download_file(dl_link)
+        print(f"downloaded file: {status}")
